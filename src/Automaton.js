3b4c97db2a765b6429e79f374ab3bce0f695b2a6
Panda (Thomas) Attwood | TickleThePanda <thomas.pg.attwood@gmail.com>
2021-06-03 11:49:18 +0100

    Pull automaton to new file

---
+++ src/Automaton.js
@@ -0,0 +1,119 @@
+export class Rule {
+  constructor(
+    start /* ref to start */,
+    condition,
+    result /* ref to result */
+  ) {
+    this.start = start;
+    this.condition = condition;
+    this.result = result;
+    this.predicate = new Function(`return ${condition}`);
+  }
+}
+
+class NeighbourCounts {
+  constructor(counts) {
+    this.counts = counts;
+  }
+
+  get(s) {
+    if (this.counts[s] === undefined) {
+      return 0;
+    }
+    return this.counts[s];
+  }
+
+}
+
+function mod(n, m) {
+  return ((n % m) + m) % m;
+}
+
+class CellProcessor {
+
+  constructor(grid, [xi, yi]) {
+    this.grid = grid;
+    this.xi = xi;
+    this.yi = yi;
+  }
+
+  countNeighbours(neighbourType) {
+    const grid = this.grid;
+    const xi = this.xi;
+    const yi = this.yi;
+
+    if (this.cachedCounts === undefined) {
+      const neighbourCounts = {};
+      for (let dx = -1; dx <= 1; dx++) {
+        for (let dy = -1; dy <= 1; dy++) {
+          if (!(dx === 0 && dy === 0)) {
+            const xim = mod((xi + dx), grid.xCount);
+            const yim = mod((yi + dy), grid.yCount);
+            const neighbourState =  grid.get(`${xim},${yim}`).state;
+            if (neighbourCounts[neighbourState] === undefined) {
+              neighbourCounts[neighbourState] = 0;
+            }
+            neighbourCounts[neighbourState]++;
+          }
+        }
+      }
+
+      this.cachedCounts = new NeighbourCounts(neighbourCounts);
+    }
+    return this.cachedCounts.get(neighbourType);
+  }
+
+  chance(p) {
+    const test = Math.random();
+  
+    return test < p ? true : false;
+  }
+}
+
+export class Automaton {
+  constructor(grid, rules, initSupplier) {
+    this.rules = rules;
+    this.initSupplier = new Function(`return ${initSupplier}`);
+    this.grid = grid;
+    this.generationCount = 0;
+  }
+  
+  initialiseGrid() {
+    for (let xi = 0; xi < this.grid.xCount; xi++) {
+      for (let yi = 0; yi < this.grid.yCount; yi++) {
+        const processor = new CellProcessor(this.grid, [xi, yi]);
+        this.grid.get(`${xi},${yi}`).state = this.initSupplier.bind(processor)();
+      }
+    }
+  }
+
+  step() {
+    console.log(`Generation ${this.generationCount++}`);
+
+    const nextGrid = [];
+
+    for (let xi = 0; xi < this.grid.xCount; xi++) {
+      for (let yi = 0; yi < this.grid.yCount; yi++) {
+        const next = this.test([xi, yi]);
+        nextGrid.push({
+          xi, yi, state: next
+        });
+      }
+    }
+
+    for (let {xi, yi, state} of nextGrid) {
+      this.grid.get(`${xi},${yi}`).state = state;
+    }
+  }
+
+  test([xi, yi]) {
+    const processor = new CellProcessor(this.grid, [xi, yi]);
+    for (let rule of this.rules) {
+      const predicateResult = rule.predicate.bind(processor)(this.grid, [xi, yi]);
+      if (predicateResult) {
+        return rule.result;
+      }
+    }
+    return this.grid.get(`${xi},${yi}`).state;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,80 +1,9 @@
 import Canvas from "./Canvas.js"
 import Grid from "./Grid.js"
+import { Automaton, Rule } from "./Automaton.js"
 
 const canvas = new Canvas();
 const grid = new Grid(canvas.width, canvas.height);
-
-class Rule {
-  constructor(
-    start /* ref to start */,
-    condition,
-    result /* ref to result */
-  ) {
-    this.start = start;
-    this.condition = condition;
-    this.result = result;
-    this.predicate = new Function(`return ${condition}`);
-  }
-}
-
-class NeighbourCounts {
-  constructor(counts) {
-    this.counts = counts;
-  }
-
-  get(s) {
-    if (this.counts[s] === undefined) {
-      return 0;
-    }
-    return this.counts[s];
-  }
-
-}
-
-function mod(n, m) {
-  return ((n % m) + m) % m;
-}
-
-class CellProcessor {
-
-  constructor(grid, [xi, yi]) {
-    this.grid = grid;
-    this.xi = xi;
-    this.yi = yi;
-  }
-
-  countNeighbours(neighbourType) {
-    const grid = this.grid;
-    const xi = this.xi;
-    const yi = this.yi;
-
-    if (this.cachedCounts === undefined) {
-      const neighbourCounts = {};
-      for (let dx = -1; dx <= 1; dx++) {
-        for (let dy = -1; dy <= 1; dy++) {
-          if (!(dx === 0 && dy === 0)) {
-            const xim = mod((xi + dx), grid.xCount);
-            const yim = mod((yi + dy), grid.yCount);
-            const neighbourState =  grid.get(`${xim},${yim}`).state;
-            if (neighbourCounts[neighbourState] === undefined) {
-              neighbourCounts[neighbourState] = 0;
-            }
-            neighbourCounts[neighbourState]++;
-          }
-        }
-      }
-
-      this.cachedCounts = new NeighbourCounts(neighbourCounts);
-    }
-    return this.cachedCounts.get(neighbourType);
-  }
-
-  chance(p) {
-    const test = Math.random();
-  
-    return test < p ? true : false;
-  }
-}
 
 const rules = [
   // if <2 neighbours then die
@@ -98,55 +27,6 @@ const rules = [
 ];
 
 const initSupplier = `this.chance(0.3) ? "🧞‍♀️" : "🧟‍♀️"`;
-
-class Automaton {
-  constructor(grid, rules, initSupplier) {
-    this.rules = rules;
-    this.initSupplier = new Function(`return ${initSupplier}`);
-    this.grid = grid;
-    this.generationCount = 0;
-  }
-  
-  initialiseGrid() {
-    for (let xi = 0; xi < grid.xCount; xi++) {
-      for (let yi = 0; yi < grid.yCount; yi++) {
-        const processor = new CellProcessor(grid, [xi, yi]);
-        this.grid.get(`${xi},${yi}`).state = this.initSupplier.bind(processor)();
-      }
-    }
-  }
-
-  step() {
-    console.log(`Generation ${this.generationCount++}`);
-
-    const nextGrid = [];
-
-    for (let xi = 0; xi < this.grid.xCount; xi++) {
-      for (let yi = 0; yi < this.grid.yCount; yi++) {
-        const next = this.test([xi, yi]);
-        nextGrid.push({
-          xi, yi, state: next
-        });
-      }
-    }
-
-    for (let {xi, yi, state} of nextGrid) {
-      this.grid.get(`${xi},${yi}`).state = state;
-    }
-  }
-
-  test([xi, yi]) {
-    const processor = new CellProcessor(this.grid, [xi, yi]);
-    for (let rule of this.rules) {
-      const predicateResult = rule.predicate.bind(processor)(this.grid, [xi, yi]);
-      if (predicateResult) {
-        return rule.result;
-      }
-    }
-    return this.grid.get(`${xi},${yi}`).state;
-  }
-}
-
 
 const automaton = new Automaton(grid, rules, initSupplier);
 automaton.initialiseGrid();
